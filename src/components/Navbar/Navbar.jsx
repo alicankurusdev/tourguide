9325c235ac77b2d5d20e3d70c8c02875191a52f9
Alican Kurus <alicankurus.dev@gmail.com>
2024-04-17 23:06:10 +0300

    navbar completed

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./Navbar.scss"
+
+
+const Navbar =()=>{
+    return(
+        <div>
+            <nav>
+                    <a href="#">ABOUT US</a>
+                    <a href="#">FOR YOU</a>
+                    <a href="#">SERVICES</a>
+                    <a href="#">BLOG</a>
+                    <a href="#">VLOG</a>
+                    <a href="#">CONTACT</a>
+            </nav>
+        </div>
+        )
+}
+
+export default Navbar;
