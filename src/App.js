b1a86cfb34552da9796dd465a227a60b0ae68ac3
Alican Kurus <alicankurus.dev@gmail.com>
2024-04-17 22:48:57 +0300

    scss configuration completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
 function App() {
   return (
     <div className="App">
-     
+
+     {/* navbar */}
+     {/* Header */}
+     {/* Main */}
+
     </div>
   );
 }
